08b7606c757c8eff02d40bef15be316cc713444d
Brian Odida <bryodiiidah@gmail.com>
2019-11-21 12:15:48 +0300

    updates text formats

---
+++ app/src/main/java/com/moringaschool/eloque/AddProjectActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.eloque;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_project);
+    }
+}
--- app/src/main/java/com/moringaschool/eloque/HomeFragment.java
+++ app/src/main/java/com/moringaschool/eloque/HomeFragment.java
@@ -1,13 +1,14 @@
 package com.moringaschool.eloque;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 
 /**
@@ -24,8 +25,16 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        CardView addProject = view.findViewById(R.id.addProject);
+        addProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AddProjectActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
