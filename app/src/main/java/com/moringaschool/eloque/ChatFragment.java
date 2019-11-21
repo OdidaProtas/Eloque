23e976e03110d2236553bc6c529b52af99a89540
Brian Odida <bryodiiidah@gmail.com>
2019-11-21 16:13:58 +0300

    formats contacts listview margin

--- app/src/main/java/com/moringaschool/eloque/ChatFragment.java
+++ app/src/main/java/com/moringaschool/eloque/ChatFragment.java
@@ -1,19 +1,43 @@
 package com.moringaschool.eloque;
 
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.moringaschool.eloque.models.ContactsInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChatFragment extends Fragment {
+    public static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    ContactsAdapter dataAdapter = null;
+    ListView listView;
+    Button btnGetContacts;
+    List<ContactsInfo> contactsInfoList;
+    private TextView info;
 
 
     public ChatFragment() {
@@ -25,7 +49,116 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        final View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+
+
+        btnGetContacts = (Button) view.findViewById(R.id.btnGetContacts);
+        listView = (ListView) view.findViewById(R.id.lstContacts);
+        info =  view.findViewById(R.id.contactsInfo);
+        listView.setAdapter(dataAdapter);
+
+        btnGetContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestContactPermission();
+                btnGetContacts.setVisibility(View.GONE);
+                info.setVisibility(View.VISIBLE);
+            }
+        });
+        return view;
+    }
+
+    private void getContacts(){
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        String contactId = null;
+        String displayName = null;
+        contactsInfoList = new ArrayList<ContactsInfo>();
+        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0) {
+
+                    ContactsInfo contactsInfo = new ContactsInfo();
+                    contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                    contactsInfo.setContactId(contactId);
+                    contactsInfo.setDisplayName(displayName);
+
+                    Cursor phoneCursor = getActivity().getContentResolver().query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{contactId},
+                            null);
+
+                    if (phoneCursor.moveToNext()) {
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                        contactsInfo.setPhoneNumber(phoneNumber);
+                    }
+
+                    phoneCursor.close();
+
+                    contactsInfoList.add(contactsInfo);
+                }
+            }
+        }
+        cursor.close();
+
+        dataAdapter = new ContactsAdapter(getContext(), R.layout.contacts_info, contactsInfoList);
+        listView.setAdapter(dataAdapter);
+    }
+
+    public void requestContactPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                        android.Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Read contacts access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please enable access to contacts.");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {android.Manifest.permission.READ_CONTACTS}
+                                    , PERMISSIONS_REQUEST_READ_CONTACTS);
+                        }
+                    });
+                    builder.show();
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{android.Manifest.permission.READ_CONTACTS},
+                            PERMISSIONS_REQUEST_READ_CONTACTS);
+                }
+            } else {
+                getContacts();
+            }
+        } else {
+            getContacts();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts();
+                } else {
+                    Toast.makeText(getContext(), "You have disabled a contacts permission", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/moringaschool/eloque/ContactsAdapter.java
@@ -0,0 +1,54 @@
+package com.moringaschool.eloque;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.moringaschool.eloque.models.ContactsInfo;
+
+import java.util.List;
+
+public class ContactsAdapter extends ArrayAdapter {
+    private List contactsInfoList;
+    private Context context;
+
+    public ContactsAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+        this.contactsInfoList = objects;
+        this.context = context;
+    }
+
+    private class ViewHolder {
+        TextView displayName;
+        TextView phoneNumber;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
+
+        if (convertView == null) {
+            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = vi.inflate(R.layout.contacts_info, null);
+
+            holder = new ViewHolder();
+            holder.displayName = (TextView) convertView.findViewById(R.id.displayName);
+            holder.phoneNumber = (TextView) convertView.findViewById(R.id.phoneNumber);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        ContactsInfo contactsInfo = (ContactsInfo) contactsInfoList.get(position);
+        holder.displayName.setText(contactsInfo.getDisplayName());
+        holder.phoneNumber.setText(contactsInfo.getPhoneNumber());
+
+        return convertView;
+    }
+}
